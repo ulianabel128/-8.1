07e0109e6f148283ee7628f5527e04455a21ef2a
user <user@LAPTOP-M9GFNJT2>
2017-09-02 13:05:06 +0500

    Добавьте файлы проекта.

---
+++ практика задание 8.1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace практика_задание_8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int r = -1;
+            bool ok = false;
+
+            do
+            {
+                Console.WriteLine("Введите размер матрицы смежности");
+                string user = Console.ReadLine();
+                ok = int.TryParse(user, out r);
+                if (r < 1) ok = false;
+                if (!ok) Console.WriteLine("Неверный ввод");
+            }
+            while (!ok);
+
+            ok = false;
+
+            int[,] matrix = new int[r, r];
+            
+            for (int i=0; i<matrix.GetLength(0); i++)
+                for (int j= 0; j < matrix.GetLength(0); j++)
+                {
+                    do
+                    {
+                        Console.WriteLine("Введите элемент матрицы " + (i + 1) + " " + (j + 1));
+                        string user = Console.ReadLine();
+                        ok = int.TryParse(user, out matrix[i,j]);
+                        if ((matrix[i, j]<0) || (matrix[i,j]>1)) ok = false;
+                        if ((i == j) && (r == 1)) ok = false;
+                        if (!ok) Console.WriteLine("Неверный ввод");
+                    }
+                    while (!ok);
+
+                    ok = false;
+                }
+        }
+    }
+}
